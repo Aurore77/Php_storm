f9d3034c2a38b24c04d3df9aad52aa53fa4b780e
Aurore77 <aurore_david77@hotmail.fr>
2016-09-21 11:32:59 +0200

    Initial commit

---
+++ list_eleves.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenti
+ * Date: 20/09/16
+ * Time: 16:21
+ */
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+
+$nbstudents=count$students;
+/*$total=array_sum($students);*/
+
+foreach ($students as $key => $age) {
+    echo $key . ' ' . $age . '<br>';
+}
+echo "La moyenne d'âge est de " . array_sum($students) / $nbstudents . " ans.";
